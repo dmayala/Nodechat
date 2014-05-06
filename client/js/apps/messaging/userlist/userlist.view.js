7393b9efe145e7fe6089e83549e35ce3e7580801
Daniel Ayala <dmayala@csupomona.edu>
2014-05-05 21:17:05 -0700

    Add No of Users indicator

--- client/js/apps/messaging/userlist/userlist.view.js
+++ client/js/apps/messaging/userlist/userlist.view.js
@@ -1,11 +1,24 @@
 ChatApp.module('MessagingApp.UserList', function(UserList, App, Backbone, Marionette, $, _) {
 
   UserList.User = Marionette.ItemView.extend({
+  	tagName: 'tr',
     template: '#userItem'
   });
 
-  UserList.Users = Marionette.CollectionView.extend({
-    itemView: UserList.User
+  UserList.Users = Marionette.CompositeView.extend({
+  	tagName: 'table',
+  	template: '#userListTemp',
+    itemView: UserList.User,
+    itemViewContainer: 'tbody',
+
+    collectionEvents: {
+    	'add': 'userNum',
+    	'remove': 'userNum'
+    },
+
+    userNum: function() {
+    	this.$('.js-usernum').html(this.collection.models.length);
+    }
   });
   
 });
