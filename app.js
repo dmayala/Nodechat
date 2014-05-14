f5a15e32f69aad815fbfc197aee29e0a40c48e5b
Daniel Ayala <dmayala@csupomona.edu>
2014-05-13 20:42:45 -0700

    Add hbs views for errors

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
+var hbs = require('hbs');
 var mongoose = require('mongoose');
 
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
-//var mongoUri = 'mongodb://localhost/nodechat-dev';
+var mongoUri = 'mongodb://localhost/nodechat-dev';
 
+app.engine('html', hbs.__express);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -30,8 +32,20 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Handle 404
+app.use(function(req, res) {
+  res.status(404);
+  res.render('404', {layout: false});
+});
+
+// Handle 500
+app.use(function(error, req, res, next) {
+  res.status(500);
+  res.render('500', {layout: false, error: error});
+});
+
 // MongoDB
-//var db = mongoose.connect(mongoUri);
+var db = mongoose.connect(mongoUri);
 
 routes(app, io);
 
