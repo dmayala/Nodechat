051dcc24d45508bf418459b37eeb752483baa75e
Daniel Ayala <dmayala@csupomona.edu>
2014-10-09 20:22:30 -0700

    Remove unused files

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var SocketHandler = require('./socket');
-
-module.exports = exports = function(app, io, db){
-  var socketHandler = new SocketHandler(io, db);
-
-  app.get('/users', socketHandler.getUsers);
-  app.get('/messages', socketHandler.getMessages);
-}
